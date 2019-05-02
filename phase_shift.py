70fb6eb84450705d7a642e782f7b97423ed90d7e
Deniz Ertuncay <dertuncay@gmail.com>
2019-05-02 11:03:44 +0200

    Update phase_shift.py

--- phase_shift.py
+++ phase_shift.py
@@ -1,9 +1,12 @@
 import numpy as np
+import scipy
 from obspy import read
 from scipy.signal import hilbert
 import matplotlib.pyplot as plt
 from waveletAnalysis import * 
 from pulse_identifier import wavelet, shift, ricker, pgv_pos_finder
+
+''' You need to update the Tp value depending on the result of pulse_identifier.py! '''
 
 def corrcoef(rec1,rec2):
   return np.corrcoef(rec1,rec2)
@@ -46,38 +49,36 @@ def ricker_with_phase(f, dt,data, phase):
   return shifted_wavelet
 
 st = read('NGA.RSN1482..HGR.SAC',format='SAC')
-st = read(sac,format='SAC')
 st[0].filter('bandpass',freqmin = 0.05, freqmax = 10)
 st[0].integrate()
-scale = scale_finder(st)
-data = st[0].data*scale
+data = st[0].data
 dt = st[0].stats.delta
 times = st[0].times()
+Tp = 5.3
 
-  # Energy drop
-  en_drop = []
+# Energy drop
+en_drop = []
+
+# Phase angles
+phase_angles = np.arange(0,356,5)
+for angle in phase_angles:
+  # Phase Shift to Ricker Wavelet
+  phase = np.deg2rad(angle)
   
-  # Phase angles
-  phase_angles = np.arange(0,356,5)
-  for angle in phase_angles:
-    # Phase Shift to Ricker Wavelet
-    phase = np.deg2rad(angle)
-    
-    # Apply phase angle to ricker wavelet
-    shifted_wavelet = ricker_with_phase(1/Tp, dt, data, phase)
-    
-    # Correlation Coefficient
-    corr_coef = corrcoef(data,shifted_wavelet)
-    #abs(corr_coef[0][1])
-    en_drop.append(corr_coef[0][1])
-    
-    max_corr = np.where(en_drop == np.array(en_drop).max())[0][0]
-    phased_ricker = ricker_with_phase(1/Tp, dt,data, np.deg2rad(phase_angles[max_corr]))
-    
-    
-  plt.plot(times,data,linewidth=1,color='k')
-  plt.plot(times,phased_ricker,linewidth=2,color='r',label=str(phase_angles[max_corr]))
-  plt.xlabel('Time (s)')
-  plt.ylabel('Amplitude (cm/s)')
-  plt.xlim(0,max(times))
-  plt.show()
+  # Apply phase angle to ricker wavelet
+  shifted_wavelet = ricker_with_phase(1/Tp, dt, data, phase)
+  
+  # Correlation Coefficient
+  corr_coef = corrcoef(data,shifted_wavelet)
+  en_drop.append(corr_coef[0][1])
+  
+  max_corr = np.where(en_drop == np.array(en_drop).max())[0][0]
+  phased_ricker = ricker_with_phase(1/Tp, dt,data, np.deg2rad(phase_angles[max_corr]))
+        
+plt.plot(times,data,linewidth=1,color='k')
+plt.plot(times,phased_ricker,linewidth=2,color='r',label=str(phase_angles[max_corr]))
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude (cm/s)')
+plt.xlim(0,max(times))
+plt.legend()
+plt.show()
